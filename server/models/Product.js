30a519f3c5c8419c90182a12bc4190998d518a2e
hyo <kdhyo98@gmail.com>
2020-07-21 12:07:52 +0900

    상품가져와서 뿌려주기

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,33 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProductSchema = mongoose.Schema({
-  writer: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
+const ProductSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+    },
+    images: {
+      type: Array,
+      default: [],
+    },
+    sold: {
+      type: Number,
+      maxlength: 100,
+      default: 0,
+    },
+    view: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    continent: {
+      type: String,
+    },
   },
-  title: {
-    type: String,
-    maxlength: 50
-  },
-  description: {
-    type: String,
-  },
-  images: {
-    type: Array,
-    default: []
-  },
-  sold: {
-    type: Number,
-    maxlength: 100,
-    default: 0
-  },
-  view: {
-    type: Number,
-    default: 0
-  }
-}, {timestapms: true})
+  { timestapms: true }
+);
 
-const Product = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model("Product", ProductSchema);
 
-module.exports = { Product }
+module.exports = { Product };
