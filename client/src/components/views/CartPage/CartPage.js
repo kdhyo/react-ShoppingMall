56284ac62c6c2cf3615b1fadf8eed28e215241d9
hyo <kdhyo98@gmail.com>
2020-08-05 22:50:46 +0900

    Paypal btn ADD

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { getCartItems, removerCartItem } from "../../../_actions/user_actions";
 import UserCardBlock from "./Sections/UserCardBlock";
 import { Empty } from "antd";
+import Paypal from "../../utils/Paypal";
 
 function CartPage(props) {
   const dispatch = useDispatch();
@@ -60,6 +61,8 @@ function CartPage(props) {
           <Empty description={false} />
         </>
       )}
+
+      <Paypal />
     </div>
   );
 }
